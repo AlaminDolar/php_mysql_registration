4f8df7aa5f2c93f9a316eac16e18d0136b06fcbb
ALAMIN Dolar <you@example.com>
2021-09-07 16:21:14 +0600

    registration form

---
+++ validation.php
@@ -0,0 +1,45 @@
+<?php
+
+session_start();
+ 
+
+
+$con= mysqli_connect('localhost','root');
+
+if($con){
+
+    echo"connection success";
+}else{
+    echo"no connection";
+}
+
+mysqli_select_db($con,'sessionpractical');
+
+$name = $_POST['user'];
+$pass = $_POST['password'];
+
+$q= "select * from signin where name='$name' && password ='$pass'";
+
+$result= mysqli_query($con,$q);
+
+$num= mysqli_num_rows($result);
+
+if($num == 1){
+    $_SESSION ['username']= $name;
+    header('location:home.php');
+}else{
+    header('location:index.html');
+}
+
+
+
+
+
+
+ 
+
+
+
+
+
+?>
